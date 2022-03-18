b74ec9f3ea36ba5f155a2bf80ce8b8a2ffd8a605
Benjamin <s204461@student.dtu.dk>
2022-03-18 11:43:13 +0100

    Add memory using maps

--- Task1.fsx
+++ Task1.fsx
@@ -18,68 +18,65 @@ open Task1Lexer
 // Toggles whether a deterministic program graph is created
 let determinism = true
 
-let variables = Map.empty
-let arrays = Map.empty
-
 // We define the evaluation function recursively, by induction on the structure
 // of arithmetic expressions (AST of type expr)
-let rec aeval e =
+let rec aeval e (var : Map<string, float>, arr : Map<string, float []>) : float =
   match e with
     | Num(x)                -> x
-    | Var(x)                ->  match Map.tryFind x variables with
+    | Var(x)                ->  match Map.tryFind x var with
                                 | Some value -> value
-                                | None -> failwith "No variable with name " + x + " exists."
-    | Array(x,y)            ->  match List.tryFindIndex (fun elm -> elm = aeval(y)) (Map.find x arrays) with
+                                | None -> 0
+                                            // failwith (sprintf "No variable with name %s exists." x)
+    | Array(x,y)            ->  match Array.tryFindIndex (fun elm -> elm = aeval y (var, arr)) (Map.find x arr) with
                                 | Some value -> value
-                                | None -> failwith "Index " + aeval(y) + " not found."
-    | TimesExpr(x,y)        -> aeval(x) * aeval(y)
-    | DivExpr(x,y)          -> aeval(x) / aeval(y)
-    | PlusExpr(x,y)         -> aeval(x) + aeval(y)
-    | MinusExpr(x,y)        -> aeval(x) - aeval(y)
-    | PowExpr(x,y)          -> aeval(x) ** aeval(y)
-    | UPlusExpr(x)          -> aeval(x)
-    | UMinusExpr(x)         -> -aeval(x)
-    | ParAExpr(x)           -> aeval(x)
-and beval e =
+                                | None -> 0
+                                            // failwith (sprintf "Index %f not found." (aeval y (var, arr)))
+    | TimesExpr(x,y)        -> aeval x (var, arr) * aeval y (var, arr)
+    | DivExpr(x,y)          -> int(aeval x (var, arr) / aeval y (var, arr))
+    | PlusExpr(x,y)         -> aeval x (var, arr) + aeval y (var, arr)
+    | MinusExpr(x,y)        -> aeval x (var, arr) - aeval y (var, arr)
+    | PowExpr(x,y)          -> aeval x (var, arr) ** aeval y (var, arr)
+    | UPlusExpr(x)          -> aeval x (var, arr)
+    | UMinusExpr(x)         -> -aeval x (var, arr)
+    | ParAExpr(x)           -> aeval x (var, arr)
+and beval e (var : Map<string, float>, arr : Map<string, float []>) =
   match e with
     | True                  -> true
     | False                 -> false
-    | And(x, y)             ->  let lhs = beval(x)
-                                let rhs = beval(y)
+    | And(x, y)             ->  let lhs = beval x (var, arr)
+                                let rhs = beval y (var, arr)
                                 lhs && rhs
-    | Or(x, y)              ->  let lhs = beval(x)
-                                let rhs = beval(y)
+    | Or(x, y)              ->  let lhs = beval x (var, arr)
+                                let rhs = beval y (var, arr)
                                 lhs || rhs
-    | SAnd(x, y)            -> beval(x) && beval(y)
-    | SOr(x, y)             -> beval(x) || beval(y)
-    | Neg(x)                -> !beval(x)
-    | Equal(x, y)           -> aeval(x) = aeval(y)
-    | NEqual(x, y)          -> aeval(x) <> aeval(y)
-    | Greater(x, y)         -> aeval(x) > aeval(y)
-    | GreaterEqual(x, y)    -> aeval(x) >= aeval(y)
-    | Less(x, y)            -> aeval(x) < aeval(y)
-    | LessEqual(x, y)       -> aeval(x) <= aeval(y)
-    | ParBExpr(x)           -> beval(x)
-and gceval e =
+    | SAnd(x, y)            -> beval x (var, arr) && beval y (var, arr)
+    | SOr(x, y)             -> beval x (var, arr) || beval y (var, arr)
+    | Neg(x)                -> not (beval x (var, arr))
+    | Equal(x, y)           -> aeval x (var, arr) = aeval y (var, arr)
+    | NEqual(x, y)          -> aeval x (var, arr) <> aeval y (var, arr)
+    | Greater(x, y)         -> aeval x (var, arr) > aeval y (var, arr)
+    | GreaterEqual(x, y)    -> aeval x (var, arr) >= aeval y (var, arr)
+    | Less(x, y)            -> aeval x (var, arr) < aeval y (var, arr)
+    | LessEqual(x, y)       -> aeval x (var, arr) <= aeval y (var, arr)
+    | ParBExpr(x)           -> beval x (var, arr)
+and gceval e (var, arr) =
   match e with
-    | BooleanGuard(x, y)    -> if beval(x) then ceval(y)
-    | GCommands(x, y)       ->  gceval(x)
-                                gceval(y)
-and ceval e =
+    | BooleanGuard(x, y)    -> if beval x (var, arr)
+                               then ceval y (var, arr)
+                               else (var, arr)
+    | GCommands(x, y)       -> let (var1, arr1) = gceval x (var, arr)
+                               gceval y (var1, arr1)
+and ceval e (var : Map<string, float>, arr : Map<string, float []>) : Map<string, float> * Map<string, float []> =
   match e with
-    | Commands(x, y)        ->  ceval(x)
-                                ceval(y)
-    | IfStatement(x)        -> gceval(x)
-    | DoStatement(x)        -> gceval(x)
-    | AssignExpr(x, y)      -> variables.Change (x, (fun elm =  match elm with
-                                                                | Some -> aeval(y)
-                                                                | None -> failwith "No variable with name " + x + " exists."))
-    | AssignArray(x, y, z)  -> match List.tryFindIndex (fun elm -> elm = aeval(y)) (Map.find x arrays) with
-                                | Some value -> value
-                                | None -> failwith "Index " + aeval(y) + " not found."
-    
-    "" + x + "[" + aeval(y) + "]" + ":=" + aeval(z)
-    | Skip                  -> " skip "
+    | Commands(x, y)        -> let (var1, arr1) = ceval x (var, arr)
+                               ceval y (var1, arr1)
+    | IfStatement(x)        -> gceval x (var, arr)
+    | DoStatement(x)        -> gceval x (var, arr)
+    | AssignExpr(x, y)      -> (var.Add (x, aeval y (var, arr)), arr)
+    | AssignArray(x, y, z)  -> let arr1 = Map.find x arr
+                               arr1.[int(aeval y (var, arr))] <- aeval z (var, arr)
+                               (var, arr.Add (x, arr1))
+    | Skip                  -> (var, arr)
 
 type Node = | Start
             | End
@@ -94,6 +91,7 @@ let printEdge qFrom label qTo = qFrom.ToString() + " -> " + qTo.ToString() + " [
 
 let mutable n = 0
 
+(*
 let getFresh q = match q with
                  | Start|Inter _ -> n <- n + 1
                                     Inter n
@@ -139,6 +137,7 @@ and edgesGC qFrom qTo GC b =
                                             | _ ->  let E1 = edgesGC qFrom qTo gc1 b
                                                     let E2 = edgesGC qFrom qTo gc2 b
                                                     E1 @ E2
+*)
 
 let parse input =
     // translate string into a buffer of characters
@@ -158,22 +157,31 @@ let compute =
     printf "Enter an input program: "
 
     // We parse the input string
-    try
-    let c = parse (Console.ReadLine())       
-    let edges = edgesC Start End c
+    // try
+    let c = parse (Console.ReadLine())
+    let (var, arr) = ceval c (Map.empty, Map.empty)
+
+    let s = Map.fold (fun state n s -> state + string n + " " + string s + "\n") "" var
+    printfn "%s" s
+
+    // printfn "Map of variables: %M" var
+
+    // let edges = edgesC Start End c
     // printfn "List of edges: %A" edges
 
+    (*
     printfn "digraph program_graph {rankdir=LR;"
     printfn "node [shape = circle]; qS;"
     printfn "node [shape = doublecircle]; qE;"
     printfn "node [shape = circle]"
     printfn "%s" (printEdges edges)
     printfn "}"
+    *)
 
     // and print the result of evaluating it        
     //printfn "Result: \n%s" (ceval c 0)
 
-    with err -> printf "Couldn't parse program"
+    // with err -> printf "Couldn't parse program"
 
 // Start interacting with the user
 compute
